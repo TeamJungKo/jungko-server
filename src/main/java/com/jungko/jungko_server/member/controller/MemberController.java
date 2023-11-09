989dd38a0654b38005d64a7bdc72b6608bc99ad1
sichoi42 <42.4.sichoi@gmail.com>
2023-11-09 10:35:25 +0900

    [BE] FEAT: 컨트롤러에서 로그인 회원 정보를 받도록 수정 #TSK-55

--- src/main/java/com/jungko/jungko_server/member/controller/MemberController.java
+++ src/main/java/com/jungko/jungko_server/member/controller/MemberController.java
@@ -1,5 +1,8 @@
 package com.jungko.jungko_server.member.controller;
 
+import com.jungko.jungko_server.auth.annotation.LoginMemberInfo;
+import com.jungko.jungko_server.auth.domain.MemberRole;
+import com.jungko.jungko_server.auth.dto.MemberSessionDto;
 import com.jungko.jungko_server.member.dto.request.MemberProfileUpdateRequestDto;
 import com.jungko.jungko_server.member.dto.response.MemberProfileResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import javax.validation.Valid;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -24,35 +28,42 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "회원", description = "회원 관련 API")
 public class MemberController {
 
-    @Operation(summary = "내 프로필 조회", description = "내 프로필을 조회합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "ok"),
-    })
-    @GetMapping(value = "/me/profile")
-    public MemberProfileResponseDto getMyProfile() {
-        log.info("Called getMyProfile");
-        return MemberProfileResponseDto.builder().build();
-    }
+	@Operation(summary = "내 프로필 조회", description = "내 프로필을 조회합니다.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "ok"),
+	})
+	@GetMapping(value = "/me/profile")
+	@Secured(MemberRole.S_USER)
+	public MemberProfileResponseDto getMyProfile(
+			@LoginMemberInfo MemberSessionDto memberSessionDto
+	) {
+		log.info("Called getMyProfile member: {}", memberSessionDto);
+		return MemberProfileResponseDto.builder().build();
+	}
 
-    @Operation(summary = "내 프로필 수정", description = "내 프로필 정보를 수정합니다. null이 아닌 필드에 대해서만 수정이 이루어집니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "ok"),
-    })
-    @PatchMapping(value = "/me/profile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public void updateCard(
-            @Valid @ModelAttribute MemberProfileUpdateRequestDto dto,
-            @PathVariable("memberId") Long memberId) {
-        log.info("Called updateCard dto: {}", dto);
-    }
+	@Operation(summary = "내 프로필 수정", description = "내 프로필 정보를 수정합니다. null이 아닌 필드에 대해서만 수정이 이루어집니다.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "ok"),
+	})
+	@PatchMapping(value = "/me/profile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	@Secured(MemberRole.S_USER)
+	public void updateCard(
+			@LoginMemberInfo MemberSessionDto memberSessionDto,
+			@Valid @ModelAttribute MemberProfileUpdateRequestDto dto
+	) {
+		log.info("Called updateCard member: {}, dto: {}", memberSessionDto, dto);
+	}
 
-    @Operation(summary = "회원 프로필 조회", description = "특정 회원의 프로필을 조회합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "ok"),
-    })
-    @GetMapping(value = "/{memberId}/profile")
-    public MemberProfileResponseDto getMemberProfile(
-            @PathVariable("memberId") Long memberId) {
-        log.info("Called getMemberProfile memberId: {}", memberId);
-        return MemberProfileResponseDto.builder().build();
-    }
+	@Operation(summary = "회원 프로필 조회", description = "특정 회원의 프로필을 조회합니다.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "ok"),
+	})
+	@GetMapping(value = "/{memberId}/profile")
+	@Secured(MemberRole.S_USER)
+	public MemberProfileResponseDto getMemberProfile(
+			@LoginMemberInfo MemberSessionDto memberSessionDto,
+			@PathVariable("memberId") Long memberId) {
+		log.info("Called getMemberProfile member: {}, memberId: {}", memberSessionDto, memberId);
+		return MemberProfileResponseDto.builder().build();
+	}
 }
--- src/main/java/com/jungko/jungko_server/product/controller/ProductController.java
+++ src/main/java/com/jungko/jungko_server/product/controller/ProductController.java
@@ -1,6 +1,9 @@
 package com.jungko.jungko_server.product.controller;
 
 import com.jungko.jungko_server.area.dto.response.AreaListResponseDto;
+import com.jungko.jungko_server.auth.annotation.LoginMemberInfo;
+import com.jungko.jungko_server.auth.domain.MemberRole;
+import com.jungko.jungko_server.auth.dto.MemberSessionDto;
 import com.jungko.jungko_server.product.dto.response.ProductCategoryListResponseDto;
 import com.jungko.jungko_server.product.dto.response.ProductDetailResponseDto;
 import com.jungko.jungko_server.product.dto.response.ProductListResponseDto;
@@ -12,6 +15,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,76 +30,94 @@ import org.springframework.data.domain.Sort.Order;
 @Tag(name = "상품", description = "상품 관련 API")
 public class ProductController {
 
-        @Operation(summary = "검색어를 통해 상품 목록을 조회", description = "상세 검색 페이지, 제목과 카테고리, 지역, 가격범위를 기반으로하여 상품을 조회합니다. 제목과 일치하는 결과를 제공합니다.")
-        @ApiResponses(value = {
-                        @ApiResponse(responseCode = "200", description = "ok"),
-        })
-        @GetMapping(value = "/search")
-        public ProductListResponseDto searchProducts(
-                        @RequestParam String query,
-                        @RequestParam Integer page,
-                        @RequestParam Integer size,
-                        @RequestParam String sort,
-                        @RequestParam Order order) {
-                log.info("Called searchProducts query: {}, page: {}, size: {}, sort: {}, order{}", query, page, size,
-                                sort,
-                                order);
-                return ProductListResponseDto.builder().build();
-        }
+	@Operation(summary = "검색어를 통해 상품 목록을 조회", description = "상세 검색 페이지, 제목과 카테고리, 지역, 가격범위를 기반으로하여 상품을 조회합니다. 제목과 일치하는 결과를 제공합니다.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "ok"),
+	})
+	@GetMapping(value = "/search")
+	@Secured(MemberRole.S_USER)
+	public ProductListResponseDto searchProducts(
+			@LoginMemberInfo MemberSessionDto memberSessionDto,
+			@RequestParam String query,
+			@RequestParam Integer page,
+			@RequestParam Integer size,
+			@RequestParam String sort,
+			@RequestParam Order order) {
+		log.info(
+				"Called searchProducts member: {}, query: {}, page: {}, size: {}, sort: {}, order: {}",
+				memberSessionDto, query,
+				page, size,
+				sort,
+				order);
+		return ProductListResponseDto.builder().build();
+	}
 
-        @Operation(summary = "검색 결과에 대한 연관 검색어 조회", description = "현재 검색 결과의 연관 검색어를 제공합니다.")
-        @ApiResponses(value = {
-                        @ApiResponse(responseCode = "200", description = "ok"),
-        })
-        @GetMapping(value = "/search/related")
-        public RelatedQueryListResponseDto searchByRelatedKeyword(
-                        @RequestParam String query) {
-                log.info("Called searchByRelatedKeyword query: {}", query);
-                return RelatedQueryListResponseDto.builder().build();
-        }
+	@Operation(summary = "검색 결과에 대한 연관 검색어 조회", description = "현재 검색 결과의 연관 검색어를 제공합니다.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "ok"),
+	})
+	@GetMapping(value = "/search/related")
+	@Secured(MemberRole.S_USER)
+	public RelatedQueryListResponseDto searchByRelatedKeyword(
+			@LoginMemberInfo MemberSessionDto memberSessionDto,
+			@RequestParam String query) {
+		log.info("Called searchByRelatedKeyword member: {}, query: {}", memberSessionDto, query);
+		return RelatedQueryListResponseDto.builder().build();
+	}
 
-        @Operation(summary = "선택한 상품들 비교", description = "선택한 상품들을 비교합니다.")
-        @ApiResponses(value = {
-                        @ApiResponse(responseCode = "200", description = "ok"),
-        })
-        @GetMapping(value = "/compare")
-        public ProductListResponseDto compareProducts(
-                        @RequestParam Integer page,
-                        @RequestParam Integer size,
-                        @RequestParam String sort,
-                        @RequestParam Order order) {
-                log.info("Called compareProducts page: {}, size: {}, sort: {}, order: {}", page, size, sort, order);
-                return ProductListResponseDto.builder().build();
-        }
+	@Operation(summary = "선택한 상품들 비교", description = "선택한 상품들을 비교합니다.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "ok"),
+	})
+	@GetMapping(value = "/compare")
+	@Secured(MemberRole.S_USER)
+	public ProductListResponseDto compareProducts(
+			@LoginMemberInfo MemberSessionDto memberSessionDto,
+			@RequestParam Integer page,
+			@RequestParam Integer size,
+			@RequestParam String sort,
+			@RequestParam Order order) {
+		log.info("Called compareProducts member: {}, page: {}, size: {}, sort: {}, order: {}",
+				memberSessionDto, page, size, sort, order);
+		return ProductListResponseDto.builder().build();
+	}
 
-        @Operation(summary = "특정 상품 상세 정보 조회", description = "특정 상품의 상세 정보를 조회합니다.")
-        @ApiResponses(value = {
-                        @ApiResponse(responseCode = "200", description = "ok"),
-        })
-        @GetMapping(value = "/{productId}")
-        public ProductDetailResponseDto getProductDetail(
-                        @PathVariable("productId") Long productId) {
-                log.info("Called getProductDetail productId: {}", productId);
-                return ProductDetailResponseDto.builder().build();
-        }
+	@Operation(summary = "특정 상품 상세 정보 조회", description = "특정 상품의 상세 정보를 조회합니다.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "ok"),
+	})
+	@GetMapping(value = "/{productId}")
+	@Secured(MemberRole.S_USER)
+	public ProductDetailResponseDto getProductDetail(
+			@LoginMemberInfo MemberSessionDto memberSessionDto,
+			@PathVariable("productId") Long productId) {
+		log.info("Called getProductDetail member: {}, productId: {}", memberSessionDto, productId);
+		return ProductDetailResponseDto.builder().build();
+	}
 
-        @Operation(summary = "전체 카테고리 목록 조회", description = "전체 카테고리 목록을 조회합니다. 리소스 수가 많지 않으므로 페이지네이션을 지원하지 않습니다.")
-        @ApiResponses(value = {
-                        @ApiResponse(responseCode = "200", description = "ok"),
-        })
-        @GetMapping(value = "/categories")
-        public ProductCategoryListResponseDto getAllCategories() {
-                log.info("Called getAllCategories");
-                return ProductCategoryListResponseDto.builder().build();
-        }
+	@Operation(summary = "전체 카테고리 목록 조회", description = "전체 카테고리 목록을 조회합니다. 리소스 수가 많지 않으므로 페이지네이션을 지원하지 않습니다.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "ok"),
+	})
+	@GetMapping(value = "/categories")
+	@Secured(MemberRole.S_USER)
+	public ProductCategoryListResponseDto getAllCategories(
+			@LoginMemberInfo MemberSessionDto memberSessionDto
+	) {
+		log.info("Called getAllCategories member: {}", memberSessionDto);
+		return ProductCategoryListResponseDto.builder().build();
+	}
 
-        @Operation(summary = "전체 지역 목록 조회", description = "전체 지역 목록을 조회합니다. 리소스 수가 많지 않으므로 페이지네이션을 지원하지 않습니다.")
-        @ApiResponses(value = {
-                        @ApiResponse(responseCode = "200", description = "ok"),
-        })
-        @GetMapping(value = "/areas")
-        public AreaListResponseDto getAllAreas() {
-                log.info("Called getAllAreas");
-                return AreaListResponseDto.builder().build();
-        }
+	@Operation(summary = "전체 지역 목록 조회", description = "전체 지역 목록을 조회합니다. 리소스 수가 많지 않으므로 페이지네이션을 지원하지 않습니다.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "ok"),
+	})
+	@GetMapping(value = "/areas")
+	@Secured(MemberRole.S_USER)
+	public AreaListResponseDto getAllAreas(
+			@LoginMemberInfo MemberSessionDto memberSessionDto
+	) {
+		log.info("Called getAllAreas member: {}", memberSessionDto);
+		return AreaListResponseDto.builder().build();
+	}
 }
