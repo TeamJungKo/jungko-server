56f5ae8e6aa4dfe0db0d568e4791eb4607965de9
sichoi42 <42.4.sichoi@gmail.com>
2023-11-11 18:06:19 +0900

    [BE] FEAT: 특정 유저 프로필 조회 기능 추가 & 예외 핸들링 #TSK-59

--- src/main/java/com/jungko/jungko_server/member/service/MemberService.java
+++ src/main/java/com/jungko/jungko_server/member/service/MemberService.java
@@ -4,6 +4,7 @@ import com.jungko.jungko_server.mapper.MemberMapper;
 import com.jungko.jungko_server.member.domain.Member;
 import com.jungko.jungko_server.member.dto.MemberProfileDto;
 import com.jungko.jungko_server.member.infrastructure.MemberRepository;
+import com.jungko.jungko_server.util.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,7 +22,15 @@ public class MemberService {
 		log.info("Called getMyProfile member: {}", loginMemberId);
 
 		Member member = memberRepository.findById(loginMemberId).orElseThrow(
-				() -> new IllegalArgumentException("해당 회원이 존재하지 않습니다. id=" + loginMemberId));
+				() -> new NotFoundException("해당 회원이 존재하지 않습니다. id=" + loginMemberId));
+		return memberMapper.toMemberProfileDto(member, member.getProfileImageUrl());
+	}
+
+	public MemberProfileDto getMemberProfile(Long memberId) {
+		log.info("Called getMemberProfile member: {}", memberId);
+
+		Member member = memberRepository.findById(memberId).orElseThrow(
+				() -> new NotFoundException("해당 회원이 존재하지 않습니다. id=" + memberId));
 		return memberMapper.toMemberProfileDto(member, member.getProfileImageUrl());
 	}
 }
