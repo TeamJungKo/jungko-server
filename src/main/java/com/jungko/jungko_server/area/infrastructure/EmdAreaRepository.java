2b438ad1c2a65809860178a3ed821a40020cb9d2
sichoi42 <42.4.sichoi@gmail.com>
2023-11-18 15:22:41 +0900

    [BE] FIX: CardPreviewDto에서 특정 지역 정보를 반환하도록 수정 #TSK-42

--- src/main/java/com/jungko/jungko_server/area/infrastructure/EmdAreaRepository.java
+++ src/main/java/com/jungko/jungko_server/area/infrastructure/EmdAreaRepository.java
@@ -1,8 +1,16 @@
 package com.jungko.jungko_server.area.infrastructure;
 
 import com.jungko.jungko_server.area.domain.EmdArea;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 public interface EmdAreaRepository extends JpaRepository<EmdArea, Long> {
+
+	@Query("SELECT ea FROM EmdArea ea "
+			+ "JOIN ea.siggArea sa "
+			+ "JOIN sa.sidoArea sia "
+			+ "WHERE ea.id = :emdAreaId")
+	Optional<EmdArea> findEmdAreaByIdWithSiggAreaWithSidoArea(Long emdAreaId);
 }
--- src/main/java/com/jungko/jungko_server/card/dto/CardPreviewDto.java
+++ src/main/java/com/jungko/jungko_server/card/dto/CardPreviewDto.java
@@ -1,8 +1,9 @@
 package com.jungko.jungko_server.card.dto;
 
+import com.jungko.jungko_server.area.dto.SpecificAreaDto;
+import com.jungko.jungko_server.product.dto.ProductCategoryDto;
 import java.time.LocalDateTime;
 
-import com.jungko.jungko_server.area.dto.AreaDto;
 import com.jungko.jungko_server.card.domain.CardScope;
 import com.jungko.jungko_server.member.dto.MemberProfileDto;
 
@@ -19,30 +20,33 @@ import lombok.ToString;
 @Schema(description = "카드 Preview를 담는 DTO")
 public class CardPreviewDto {
 
-  @Schema(description = "카드 ID", example = "1")
-  private final Long cardId;
+	@Schema(description = "카드 ID", example = "1")
+	private final Long cardId;
 
-  @Schema(description = "카드 제목", example = "제목")
-  private final String title;
+	@Schema(description = "카드 제목", example = "제목")
+	private final String title;
 
-  @Schema(description = "카드 검색 키워드", example = "검색 키워드")
-  private final String keyword;
+	@Schema(description = "카드 검색 키워드", example = "검색 키워드")
+	private final String keyword;
 
-  @Schema(description = "최소 가격", example = "10000")
-  private final Integer minPrice;
+	@Schema(description = "최소 가격", example = "10000")
+	private final Integer minPrice;
 
-  @Schema(description = "최대 가격", example = "20000")
-  private final Integer maxPrice;
+	@Schema(description = "최대 가격", example = "20000")
+	private final Integer maxPrice;
 
-  @Schema(description = "카드 공개 범위", example = "PUBLIC")
-  private final CardScope scope;
+	@Schema(description = "카드 공개 범위", example = "PUBLIC")
+	private final CardScope scope;
 
-  @Schema(description = "카드 생성 시간", example = "2021-08-01T00:00:00")
-  private final LocalDateTime createdAt;
+	@Schema(description = "카드 생성 시간", example = "2021-08-01T00:00:00")
+	private final LocalDateTime createdAt;
 
-  @Schema(description = "카드 작성자 정보", implementation = MemberProfileDto.class)
-  private final MemberProfileDto author;
+	@Schema(description = "카드 작성자 정보", implementation = MemberProfileDto.class)
+	private final MemberProfileDto author;
 
-  @Schema(description = "지역 정보", implementation = AreaDto.class)
-  private final AreaDto area;
+	@Schema(description = "지역 정보", implementation = SpecificAreaDto.class)
+	private final SpecificAreaDto area;
+
+	@Schema(description = "카테고리 정보", implementation = ProductCategoryDto.class)
+	private final ProductCategoryDto category;
 }
