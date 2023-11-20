f5f2b7bc804e4df2cc254473a0efe39609d995bb
Inseong Choi <tomas1997@naver.com>
2023-11-20 16:59:02 +0900

    [BE]FEAT: 상품들간 비교 기능 API 초기 구현 #TSK-32

--- src/main/java/com/jungko/jungko_server/product/service/ProductService.java
+++ src/main/java/com/jungko/jungko_server/product/service/ProductService.java
@@ -1,21 +1,15 @@
 package com.jungko.jungko_server.product.service;
 
-import com.jungko.jungko_server.area.domain.EmdArea;
 import com.jungko.jungko_server.area.domain.SidoArea;
-import com.jungko.jungko_server.area.domain.SiggArea;
-import com.jungko.jungko_server.area.dto.AreaDto;
-import com.jungko.jungko_server.area.dto.SidoDto;
 import com.jungko.jungko_server.area.dto.response.AreaListResponseDto;
-import com.jungko.jungko_server.area.infrastructure.EmdAreaRepository;
 import com.jungko.jungko_server.area.infrastructure.SidoAreaRepository;
-import com.jungko.jungko_server.area.infrastructure.SiggAreaRepository;
 import com.jungko.jungko_server.mapper.AreaMapper;
 import com.jungko.jungko_server.mapper.ProductMapper;
 import com.jungko.jungko_server.product.domain.Product;
-import com.jungko.jungko_server.product.domain.ProductCategory;
 import com.jungko.jungko_server.product.dto.ProductCategoryDto;
 import com.jungko.jungko_server.product.dto.ProductDetailDto;
 import com.jungko.jungko_server.product.dto.response.ProductCategoryListResponseDto;
+import com.jungko.jungko_server.product.dto.response.ProductListResponseDto;
 import com.jungko.jungko_server.product.infrastructure.ProductCategoryRepository;
 import com.jungko.jungko_server.product.infrastructure.ProductRepository;
 import java.util.stream.Collectors;
@@ -25,7 +19,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.HttpClientErrorException;
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -40,6 +33,14 @@ public class ProductService {
 	private final ProductCategoryRepository productCategoryRepository;
 	private final SidoAreaRepository sidoAreaRepository;
 	private final AreaMapper areaMapper;
+
+	public ProductListResponseDto compareProduct(List<Long> productIds) {
+		log.info("Called compareProduct productIds: {}", productIds);
+
+		List<Product> products = productRepository.findAllById(productIds);
+
+		return ProductListResponseDto.builder().build();
+	}
 
 	public ProductDetailDto getProductDetail(Long productId) {
 		log.info("Called getProductDetail productId: {}", productId);
