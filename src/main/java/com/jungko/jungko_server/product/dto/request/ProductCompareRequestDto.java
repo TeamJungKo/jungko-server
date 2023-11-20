eed114d0b1254fc33d0ec124cf51c6da2c922ad4
Inseong Choi <tomas1997@naver.com>
2023-11-20 16:58:12 +0900

    [BE]FIX:ProductCompareRequestDto 추가

---
+++ src/main/java/com/jungko/jungko_server/product/dto/request/ProductCompareRequestDto.java
@@ -0,0 +1,19 @@
+package com.jungko.jungko_server.product.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+@Builder
+@Schema(description = "상품들 비교하는 DTO")
+public class ProductCompareRequestDto {
+
+	@Schema(description = "상품Id", example = "1")
+	private final List<Long> productIds;
+}
