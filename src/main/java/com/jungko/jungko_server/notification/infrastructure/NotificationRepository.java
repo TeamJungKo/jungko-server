f93fcbdc7b0490d6c4f723dcc86bb7563f2f6b49
Inseong Choi <tomas1997@naver.com>
2023-11-23 12:13:13 +0900

    [BE]FEAT: 키워드 알림 전체 조회 로직 #TSK-35

--- src/main/java/com/jungko/jungko_server/notification/infrastructure/NotificationRepository.java
+++ src/main/java/com/jungko/jungko_server/notification/infrastructure/NotificationRepository.java
@@ -1,8 +1,12 @@
 package com.jungko.jungko_server.notification.infrastructure;
 
 import com.jungko.jungko_server.notification.domain.Notification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+	Page<Notification> findAllByMemberId(Long memberId, Pageable pageable);
 }
--- src/main/java/com/jungko/jungko_server/notification/service/NotificationService.java
+++ src/main/java/com/jungko/jungko_server/notification/service/NotificationService.java
@@ -1,16 +1,43 @@
 package com.jungko.jungko_server.notification.service;
 
+import com.jungko.jungko_server.member.domain.Member;
 import com.jungko.jungko_server.member.infrastructure.MemberRepository;
+import com.jungko.jungko_server.notification.domain.Notification;
+import com.jungko.jungko_server.notification.dto.KeywordNoticeDto;
+import com.jungko.jungko_server.notification.dto.response.KeywordNoticeListResponseDto;
 import com.jungko.jungko_server.notification.infrastructure.NotificationRepository;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import java.util.List;
 
 
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
 
-    private final NotificationRepository notificationRepository;
-    private final MemberRepository memberRepository;
+	private final NotificationRepository notificationRepository;
+	private final MemberRepository memberRepository;
 
+
+	public KeywordNoticeListResponseDto getNoticesByKeyword(Long memberId,
+			PageRequest pageRequest) {
+
+		Member loginMember = memberRepository.findById(memberId).orElseThrow(
+				() -> new HttpClientErrorException(
+						HttpStatus.NOT_FOUND,
+						"해당 회원이 존재하지 않습니다. id=" + memberId));
+
+		// Repository에 KEYWORD타입인 notice만 가져오도록 Query문 작성 필요
+		Page<Notification> notices = notificationRepository
+				.findAllByMemberId(memberId, pageRequest);
+
+		List<KeywordNoticeDto> keywordNoticeDtos;
+
+		return null;
+	}
 }
